4100aed26fb53d9e2080e71d3351b4d44ed89191
Nikhil Bhintade <29760727+nikbhintade@users.noreply.github.com>
2024-09-10 13:24:35 +0000

    feat(token): mint & burn

--- src/EqSei.sol
+++ src/EqSei.sol
@@ -1,6 +1,48 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.24;
 
-contract EqSei {
-    constructor() {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract EqSei is ERC20, Ownable {
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    event EqSei__TokenMinted(uint256 value);
+    event EqSei__TokenBurned(uint256 value);
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) Ownable(msg.sender) {}
+
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice This function mints the token to address depositing tokens in liquid staking contract.
+     * @dev The amount of token to mint will depend on the exchange rate of $EQSEI<>$SEI which will be calculated in liquid staking contract.
+     * @param account address of user depositing $SEI in the protocol
+     * @param value amount of $EQSEI to mint
+     */
+    function mint(address account, uint256 value) external onlyOwner {
+        _mint(account, value);
+
+        emit EqSei__TokenMinted(value);
+    }
+
+    /**
+     * @notice This function burns the $EQSEI and reduces the total supply of $EQSEI.
+     * @dev the burn will happen when user withdraws $SEI from liquid staking contract and not when they put undelegate request.
+     * @param account address of user burning $EQSEI
+     * @param value amount of $EQSEI to burn
+     */
+    function burn(address account, uint256 value) external onlyOwner {
+        _burn(account, value);
+
+        emit EqSei__TokenBurned(value);
+    }
 }
